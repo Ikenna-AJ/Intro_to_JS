e5c16d475349d99a608f5339cc61bb0451b7cc34
joeya <joeyanegbe@icloud.com>
2024-06-03 12:43:01 +0100

    Add intro javaScript code finished version

---
+++ start.js
@@ -0,0 +1,35 @@
+const a=26, b=56, c=1;
+
+// largest=Math.max(a,b,c);
+
+// console.log(largest);
+ 
+
+
+const d=345, e=1016, f=229
+ function arrayWayLargest(d,e,f) {
+    let highest=a;
+    let numbers=[d,e,f];
+    numbers.forEach((number)=>{
+        if(number>highest){
+            highest=number
+        }
+    });
+    
+return highest;
+}
+
+console.log(arrayWayLargest(d,e,f))
+
+
+
+
+// largest=getLargest(a, b, c);
+// console.log(largest);
+
+// NOTES
+// Read about object destructuring and arrays
+
+
+
+
